cb980fea3d70db8cba001684f68d1bac71a7d720
facundofumaneri <facundo@argeniss.com>
2025-03-27 15:35:24 -0300

    add some concurrency utils

---
+++ apps/walkdir/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ffumaneri/github-cli/common"
+	"time"
+)
+
+// funcion main de prueba
+func main() {
+	ws := &common.FS{}
+	t := time.Now()
+	err := ws.WalkDir("/Users/facundofumaneri/personal", func(path string, size int64) {
+		fmt.Printf("%s %d\n", path, size)
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("time: %s\n", time.Since(t))
+}
--- common/fs.go
+++ common/fs.go
@@ -2,8 +2,9 @@ package common
 
 import (
 	"fmt"
-	"log"
+	"github.com/ffumaneri/github-cli/concurrency"
 	"os"
+	"time"
 )
 
 type WalkDirCallback func(path string, size int64)
@@ -22,15 +23,18 @@ func (fs *FS) WalkDir(path string, callback WalkDirCallback) error {
 	if fs.maxLevel == 0 {
 		fs.maxLevel = DefaultMaxLevel
 	}
-
-	err := fs.walkDir(path, callback, 0)
-	if err != nil {
-		return err
-	}
+	wp := concurrency.NewWorkerPool(10)
+	wp.Start()
+	wp.AddTask(concurrency.Executor{func() error {
+		return fs.walkDir(wp, path, callback, 0)
+	}, func(err error) {
+		fmt.Println(err)
+	}})
+	wp.WaitForTimeout(1 * time.Millisecond)
 	return nil
 }
 
-func (fs *FS) walkDir(path string, callback WalkDirCallback, level int) error {
+func (fs *FS) walkDir(wp *concurrency.WorkerPool, path string, callback WalkDirCallback, level int) error {
 	if level > fs.maxLevel {
 		return nil
 	}
@@ -50,17 +54,20 @@ func (fs *FS) walkDir(path string, callback WalkDirCallback, level int) error {
 	if err != nil {
 		return err
 	}
-
+	var children []string
 	for _, file := range files {
-		log.Printf("%s%s%s", path, "/", file.Name())
 		if !file.IsDir() {
 			callback(fmt.Sprintf("%s%s%s", path, "/", file.Name()), file.Size())
 		} else {
-			err := fs.walkDir(fmt.Sprintf("%s%s%s", path, "/", file.Name()), callback, level+1)
-			if err != nil {
-				return err
-			}
+			children = append(children, fmt.Sprintf("%s%s%s", path, "/", file.Name()))
 		}
+	}
+	for _, child := range children {
+		wp.AddTask(concurrency.Executor{func() error {
+			return fs.walkDir(wp, child, callback, level+1)
+		}, func(err error) {
+			fmt.Println(err)
+		}})
 	}
 
 	return nil
